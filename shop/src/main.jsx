2655799dcb65c477b98fcf41e28618ac84984692
razcoen <raz@sprkl.dev>
2023-01-09 16:21:54 +0200

    feat: poc frontend, replace TOKEN_HERE and ID_HERE

--- shop/src/main.jsx
+++ shop/src/main.jsx
@@ -11,6 +11,7 @@ import Product from "./routes/product";
 import Cart from "./routes/cart";
 import "normalize.css";
 import "./index.scss";
+import "./sprkl";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root")
